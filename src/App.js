cdd99d6c46873b229650e46e97389972e0566261
unknown <cmkoivu@comcast.net>
2018-12-30 09:19:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
+import React from 'react';
 import './App.css';
+import Users from './Components/Users';
 
-class App extends Component {
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //Initialize the state in the constructor
+
+    this.state = {
+      users: []   
+    };
+    this.handleRoleChange = this.handleRoleChange.bind(this);
+  }
+
+  /**
+   * receives an array of users and sets the role to 
+   * the passed role argument of the selected user
+   * passed through the index, since find()
+   * is not compatible with IE.
+   * @param {*} users array of users
+   * @param {*} role new role name for the user
+   * @param {*} index the index of the element we want to change
+   */
+  changeRole(users, role, index){
+    for (let i = 0; i < users.length; i++) {
+      let match = (users[i].index === index);
+      if (match) {
+        users[i].role = role;
+        return users;
+      }
+    }
+  }
+
+  handleRoleChange(role, index) {
+    let users = this.state.users;
+
+    // change the role of the desired user by index
+    let newUsersArray = this.changeRole(users, role, index);
+
+    // test output
+    console.log(newUsersArray)
+
+    // save the updated array
+    this.setState(
+      {users: newUsersArray}
+    );
+  }
+
+  componentWillMount(){
+    /*axios.get('/api/users').then(response => {
+            this.setState({
+              users: response.data
+            })
+     });*/
+
+  
+    this.setState({
+      users: [
+        {
+          index: 1,
+          name: "Fred",
+          email: "fred@google.com",
+          role: "default"
+        },
+        {
+          index: 2,
+          name: "Mark",
+          email: "fred@google.com",
+          role: "default"
+        },
+        {
+          index: 3,
+          name: "David",
+          email: "fred@google.com",
+          role: "default"
+        }
+      ]
+    });
+  
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <Users users={this.state.users} onRoleChange={this.handleRoleChange} />
+        
       </div>
     );
-  }
+}
 }
 
 export default App;
---
+++ src/Components/ChangeRole.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class ChangeRole extends Component {
+ 
+  constructor(props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    roles: [
+      'default', 'volunteer', 'admin'
+    ]
+  }
+
+  handleSubmit(e){
+    this.props.changeRole('admin', 2);
+    //console.log('save button clicked');
+    e.preventDefault();
+  }
+
+  render() {
+    let roleOptions = this.props.roles.map(role=> {
+      return <option key = {role} value={role}>{role}</option>
+    });
+   
+    return (
+      
+        <form onSubmit ={this.handleSubmit.bind(this)}>
+          <select ref = "role">
+              {roleOptions}
+          </select>
+          <input type="submit" value="Save" />
+        </form>
+       
+        
+       
+       
+    );
+  }
+}
+
+/*
+
+Users.propTypes = {
+  users: React.PropTypes.array,
+  onDelete: React.PropTypes.func
+}*/
+
+export default ChangeRole;
